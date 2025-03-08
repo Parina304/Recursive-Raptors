5931cd6ce6dd30e205a713db61624914ff1366c0
heartman <bhzhang@tamu.edu>
2025-03-08 17:16:57 -0600

    updated operDemo

--- Proj2/operDemo.cpp
+++ Proj2/operDemo.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c3591b2ceead88390d79adaa2f82ec4116770c372bb6fd9cd7580d9774a96ed
-size 7027
+oid sha256:cb8f02da1645a6a602f55aedfb1a8a374ebe3b83846afcd33c733e8056c82e49
+size 7026
