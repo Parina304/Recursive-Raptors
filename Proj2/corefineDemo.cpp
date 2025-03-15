cdfe5c0dbf37de1f5227bcc9b395e9ce7e9da497
heartman <bhzhang@tamu.edu>
2025-03-15 17:43:51 -0500

    cleanup 3

--- Proj2/corefineDemo.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bf1246768a8a23b0538ad764203ccc5171207056840e98ba583a4c74c195a6e2
-size 1594
--- Proj2/operDemo.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a462a2ba5e958dd5331cade8af00ff592e478008b19c6be3f24e9a26a5d8fb40
-size 6486
