e2f541c1542a31586cc9fe3ab74947c7cee33ce2
heartman <bhzhang@tamu.edu>
2025-03-15 20:46:31 -0500

    bug fixed, normal rotation, translation, scaling and calc centroid

--- Proj2/mesh_lib/mesh_lib.cpp
+++ Proj2/mesh_lib/mesh_lib.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bac0806ba793f3446ae5470699bd72c962ae6bf6d9a4cf4213cf89267eb2a8eb
-size 5209
+oid sha256:fc05dc5dbd2146ae130ed8b37bee5c14427dc753e111250c44bf2bfd02fb1690
+size 5206
--- Proj2/mesh_lib/mesh_oper.cpp
+++ Proj2/mesh_lib/mesh_oper.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9b2529471917107d6e72525fff1b9aa8f95b649e6da1c88b913d75672abf198
-size 1419
+oid sha256:0af68a2c5cbc72a817ef245e50d2d338fbbbcdd88d752309843542533d0a3f46
+size 1352
