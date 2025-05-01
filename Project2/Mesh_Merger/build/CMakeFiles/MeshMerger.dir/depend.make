94dd2bf278582cb285ea734de7d4166779d08073
heartman <bhzhang@tamu.edu>
2025-04-30 20:43:18 -0500

    restored Project2

---
+++ Project2/Mesh_Merger/build/CMakeFiles/MeshMerger.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MeshMerger.
+# This may be replaced when dependencies are built.
---
+++ Project2/Proj2/build/CMakeFiles/mesh_demo_union.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mesh_demo_union.dir/mesh_demo_union.cpp.obj"
+  "CMakeFiles/mesh_demo_union.dir/mesh_demo_union.cpp.obj.d"
+  "libmesh_demo_union.dll.a"
+  "mesh_demo_union.exe"
+  "mesh_demo_union.exe.manifest"
+  "mesh_demo_union.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mesh_demo_union.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
